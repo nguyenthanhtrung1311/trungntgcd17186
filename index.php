5c438d058e2ec491334b614050117bd9e47f4d02
nguyenthanhtrung1311 <51695203+nguyenthanhtrung1311@users.noreply.github.com>
2019-06-13 15:16:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,14 +6,27 @@
  <?php echo '<h1>Hello Teacher Ho Van Phi, This is my cloud app</h1>'; 
  
  ?>
+
+ <style>
+ 	#A {
+ 	 	float: left;
+ 	 }
+
+ 	#B {
+ 	 	float: left;
+ 	 }
+ </style>
  <h2>My Infomation</h2>
  <p>Name: Nguyen Thanh Trung</p>
  <p>ID: GCD17186</p>
  <p>Class: GCD0818</p>
  <p>My Full body photo</p>
- <img src="trungcloud.jpg" alt="" height="350" width="350"> 
-
-     <a href="ConnectToDB.php" target="_blank" >Connect database</a>
+ <div id="A">
+ 	<img src="trungcloud.jpg" alt="" height="350" width="350">
+ </div>
+ 
+ <div id="B">
+ 	<a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
      <a href="InsertData.php" target="_blank" >Insert data to the database</a>
      <br/>
@@ -21,5 +34,8 @@
     <br/>
     <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
 
+ </div>
+
+     
  </body>
 </html>
